93214e931a42706aa2f61398c24da2f5a06a4419
kamei000 <kame.0128@icloud.com>
2025-02-17 15:34:37 +0900

    Add files via upload

--- KS-memo.js
+++ KS-memo.js
@@ -10,6 +10,11 @@ function load() {
     function save() {
         var mydata = document.getElementById("mydata_in").value;
         localStorage.setItem('mydata', mydata);
+        
+    }
+    function save2(){
+        var mydata2 = document.getElementById("mydata_in").value;
+        localStorage.setItem('mydata2',mydata2);
     }
     function view() {
         if(!localStorage.getItem('mydata')) {
@@ -19,6 +24,8 @@ function load() {
         }
         else {
             mydata = localStorage.getItem('mydata');
-            document.getElementById("mydata_out").innerText = mydata;
+            mydata2 = localStorage.getItem('mydata2');
+            document.getElementById("mydata_out").innerText = "1 "+mydata;
+            document.getElementById("my_data_out").innerText = "2 "+mydata2;
         }
     }
